bd2f2aa29f78c1e08544d13bd932e2743e54fab1
haotianmichael <2398419426@buaa.edu.cn>
2024-10-02 18:53:54 +0800

    fix(ilog2): fix bug of sc_signal

--- dsa/cmodel/ilog2.cpp
+++ dsa/cmodel/ilog2.cpp
@@ -18,7 +18,7 @@ void ilog2::stage1() {
         stage2_log2.write(0);
     }else if(valid){
         if(stage1_v.read().range(31, 16) != 0) {
-            stage2_v.wirte(stage1_v.read().range(31, 16));
+            stage2_v.write(stage1_v.read().range(31, 16));
             stage2_log2.write(16);
         }else {
             stage2_v.write(stage1_v.read().range(15, 0));
@@ -29,7 +29,7 @@ void ilog2::stage1() {
 
 void ilog2::stage2() {
     if(rst.read()) {
-        stage3_index.write(0);
+        stage3_indx.write(0);
         stage3_log2.write(0);
     } else if(valid){
         if(stage2_v.read().range(15, 8) != 0) {
@@ -47,7 +47,7 @@ void ilog2::stage3() {
     if(rst.read()) {
         stage4_logtable_value.write(0);
     }else if(valid){
-        stage4_logtable_value.write(LogTable256[stage3_indx]);
+        stage4_logtable_value.write(LogTable256[stage3_indx].read());
     }
 }
 
@@ -55,7 +55,7 @@ void ilog2::stage4() {
     if(rst.read()) {
         stage5_log2.write(0);
     }else if(valid){
-        stage5_log2.write(stage3_log2 + stage4_logtable_value);
+        stage5_log2.write(stage3_log2.read() + stage4_logtable_value.read());
     }
 }
 
@@ -63,6 +63,6 @@ void ilog2::end() {
     if(rst.read()) {
         outlog2.write(0);
     }else if(valid){
-        outlog2.write(stage5_log2 >> 1);
+        outlog2.write(stage5_log2.read() / 2);
     }
 }
