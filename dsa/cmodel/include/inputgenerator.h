a4910a5a8f121eaccd0736cce8515d7ba0ba4e31
haotianmichael <2398419426@buaa.edu.cn>
2024-10-15 14:46:06 +0800

    feat(InputGe): expand range of input to >65

--- dsa/cmodel/include/inputgenerator.h
+++ dsa/cmodel/include/inputgenerator.h
@@ -4,13 +4,15 @@
 #include <sstream>
 #include <string>
 #define InputLaneWIDTH  65
+#define MAX_SEGLENGTH   5000
 
 SC_MODULE(InputGenerator) {
     sc_in<bool> clk;      
     sc_in<bool> rst;
-    sc_signal<sc_int<32> > ri[InputLaneWIDTH];
-    sc_signal<sc_int<32> > qi[InputLaneWIDTH];
-    sc_signal<sc_int<32> > w[InputLaneWIDTH];
+    sc_signal<sc_int<32> > UpperBound;  // for every segment
+    sc_signal<sc_int<32> > ri[MAX_SEGLENGTH];
+    sc_signal<sc_int<32> > qi[MAX_SEGLENGTH];
+    sc_signal<sc_int<32> > w[MAX_SEGLENGTH];
 
     sc_out<sc_int<32>> ri_out[InputLaneWIDTH];
     sc_out<sc_int<32>> qi_out[InputLaneWIDTH];
@@ -18,7 +20,7 @@ SC_MODULE(InputGenerator) {
 
     int cycle_count;
 
-    void shift_elements() {
+    void shift_elements_lt64() {
         while (true) {
             wait(); 
             if(!rst.read()) {
@@ -52,9 +54,53 @@ SC_MODULE(InputGenerator) {
         }
     }
 
+    void shift_elements_all() {
+        while(true) {
+            wait();
+            if(!rst.read()) {
+                if(cycle_count <= UpperBound-65) {
+                    for(int i = 0; i < InputLaneWIDTH; i ++) {
+                        ri_out[i].write(ri[i + cycle_count]);
+                        qi_out[i].write(qi[i + cycle_count]);
+                        w[i].write(w[i + cycle_count])):
+                    }
+                    cycle_count ++;
+                }else if(cycle_count > UpperBound-65 && cycle_count < UpperBound) {
+                    for(int i = 0; i < UpperBound-cycle_count; i ++) {
+                        ri_out[i].write(ri[i + cycle_count]);
+                        qi_out[i].write(qi[i + cycle_count]);
+                        w[i].write(w[i + cycle_count]);
+                    }
+                    for(int i = UpperBound-1; i >= (UpperBound-cycle_count); i --) {
+                        ri_out[i].write(static_cast<sc_int<32> >(-1));
+                        qi_out[i].write(static_cast<sc_int<32> >(-1));
+                        w[i].write(static_cast<sc_int<32> >(-1));
+                    }
+                    cycle_count ++;
+                }else {
+                    for(int i = 0; i < UpperBound; i++) {
+                         ri_out[i].write(static_cast<sc_int<32>>(-1));
+                         qi_out[i].write(static_cast<sc_int<32>>(-1));
+                         w_out[i].write(static_cast<sc_int<32>>(-1));
+                    } 
+                }
+            }else {
+                cycle_count = 0;
+                for(int i = 0; i < InputLaneWIDTH; i++) {
+                    ri_out[i].write(static_cast<sc_int<32>>(-1));
+                    qi_out[i].write(static_cast<sc_int<32>>(-1));
+                    w_out[i].write(static_cast<sc_int<32>>(-1));
+                }
+            }
+        }
+    }
+
     SC_CTOR(InputGenerator) : cycle_count(0) {
 
-        SC_THREAD(shift_elements);
+        SC_THREAD(shift_elements_lt64);
+        sensitive << clk.pos();
+
+        SC_THREAD(shift_elements_all);
         sensitive << clk.pos();
 
         // read file
@@ -62,15 +108,29 @@ SC_MODULE(InputGenerator) {
         std::ifstream infile(filename);
         std::string line;
         int i = 0; 
-        while (std::getline(infile, line) && i < InputLaneWIDTH) {
-            std::istringstream iss(line);
-            int val1, val2, val3;
-            if (iss >> val1 >> val2 >> val3) {
-                ri[i].write(val1);
-                qi[i].write(val3);
-                w[i].write(val2);
-                i++;
+        if(UpperBound <= InputLaneWIDTH){
+            while (std::getline(infile, line) && i < InputLaneWIDTH) {
+                 std::istringstream iss(line);
+                 int val1, val2, val3;
+                 if (iss >> val1 >> val2 >> val3) {
+                    ri[i].write(val1);
+                    qi[i].write(val3);
+                    w[i].write(val2);
+                    i++;
+                 }
+            }
+        }else {
+            while(std::getline(infile, line) && i < UpperBound) {
+                std::istringstream iss(line);
+                int val1, val2, val3;
+                if(iss >> val1 >> val2 >> val3) {
+                    ri[i].write(val1); 
+                    qi[i].write(val3); 
+                    w[i].write(val2); 
+                    i++;
+                }
             }
         }
     }
 };
+ 
